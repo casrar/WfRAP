8ab98323d7fff5d3d7b827af738aa24d51e582cf
Casrar <casey.j.morar@gmail.com>
2021-12-15 20:07:45 -0500

    cleaned up comments/code a little

--- wfrap.py
+++ wfrap.py
@@ -10,10 +10,11 @@
   also I can add to my Git >:).
 
   Author: Casey Morar (casrar)
-  License: no clue
+  License: mit 
   Version: 1.0.0
 
     TODO:
+        !!! BECOME REDDIT API COMPLIANT 
         !!Define several functions, general search, title search, content search(body and comments), username search are the core 
         !!Make into a class, more maintainable, can define var in constructor 
         !! make class structure make more sense, add in Oauth and such 
@@ -59,9 +60,7 @@ class Search:
     def _search_request(self, subreddit: str, listing: str) -> requests:
         try:
             url = f'https://www.reddit.com/r/{subreddit}/.json?after={listing}&limit=100' # default max limit is 100
-            request = requests.get(url, headers = {'User-agent': self.user_agent}) # I'm truly not sure what user agent header does, GB google ig
-            #print(request.status_code)
-            #request = request.json()
+            request = requests.get(url, headers = {'User-agent': self.user_agent}) # Need to read docs see what user-agent should be 
             return request.json()
         except:
             print('An Error Occured')
@@ -69,18 +68,17 @@ class Search:
     
     # Returns list of all titles in a subreddit, up to ()
     def search_all_titles(self, subreddit: str) -> list:
-        url_list = []
+        title_list = []
         request = self._search_request(subreddit=subreddit, listing='')
         while True:
             num_dist = self._request_dist(request=request)
-            # print(num_dist)
-            # change back to for loop
-            url_list.append([self._request_title(request=request, i=i) for i in range(num_dist)])
-            # print(title_list)
+            for i in range(num_dist):
+                title_list.append(self._request_title(request=request, i=i))
+
             new_listing = self._request_after(request=request)
-            # print(new_listing)
             if (str(new_listing) == 'None'):
-                return url_list
+                return title_list
+
             request = self._search_request(subreddit=subreddit, listing=new_listing)
 
     # Returns list of all url in a subreddit, up to ()
@@ -95,6 +93,7 @@ class Search:
             new_listing = self._request_after(request=request)
             if (str(new_listing) == 'None'):
                 return permalink_list
+
             request = self._search_request(subreddit=subreddit, listing=new_listing)
 
     # Returns list of all post bodies in a subreddit, up to () 
@@ -104,20 +103,13 @@ class Search:
         count = 0
         while True:
             num_dist = self._request_dist(request=request)
-            # print(num_dist)
-
-            # changed back to traditional for loop, my list comprehension was nesting my loops
             for i in range(num_dist):
                 body_list.append(self._request_body(request=request, i=i))
-            # Deprecated
-            # body_list.append([self._request_body(request=request, i=i) for i in range(num_dist)])
 
-            # print(title_list)
             new_listing = self._request_after(request=request)
-            # print(new_listing)
             if (str(new_listing) == 'None'):
-                print(count)
                 return body_list
+
             request = self._search_request(subreddit=subreddit, listing=new_listing)
 
     # Returns list of all comments in subreddit, up to ()
@@ -127,8 +119,7 @@ class Search:
         permalink_list = self._get_all_links(subreddit=subreddit)
         comments = []
         for permalink in permalink_list:
-            permalink = 'https://www.reddit.com' + permalink + '.json'
-            print(permalink)
+            permalink = 'https://www.reddit.com' + permalink + '.json' # turn into a function, maybe
             request = requests.get(permalink, headers={'User-agent' : 'test'}).json()
             self._search_all_comments(request[1]['data']['children'], comments) # turn into a function
         
@@ -150,7 +141,8 @@ class Search:
                 comment_body = request[i]['data']['body'] # turn into a function
                 comments.append(comment_body)
     
-    def test(self) -> None:
+    # for internal testing, remove when finished
+    def _test(self) -> None:
         pass
        
 
